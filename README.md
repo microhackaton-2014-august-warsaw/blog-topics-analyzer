981c1661e1ce0cd54fa2d124385397ce846457ac
Kamil Gorlo <kgorlo@gmail.com>
2014-08-09 15:14:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+blog-topics-analyzer
+=======================
+
+Twitter places analyzer, searches through tweets for places. Then analyzers send those to Collerators.
+
+INPUT
+-----------------
+
+Accepted Content-Type:
+```
+application/vnd.com.ofg.blog-topics-analyzer.v1+json
+```
+
+Hit *PUT* at: 
+
+```
+/api/{pairId}
+```
+
+with blog data:
+
+```
+TODO
+```
+
+OUTPUT
+-----------------
+
+And it will hit collectors at /{pairId} with topics from blog posts
+
+```
+TODO
+```
+
+## Build status
+[![Build Status](https://travis-ci.org/microhackaton/blog-topics-analyzer.svg?branch=master)](https://travis-ci.org/microhackaton/blog-topics-analyzer) [![Coverage Status](http://img.shields.io/coveralls/microhackaton/blog-topics-analyzer/master.svg)](https://coveralls.io/r/microhackaton/blog-topics-analyzer)
---
+++ src/main/groovy/com/ofg/blog/controller/PairIdController.groovy
@@ -0,0 +1,37 @@
+package com.ofg.blog.controller
+import com.ofg.blog.controller.topic.extractor.PropagationWorker
+import com.wordnik.swagger.annotations.Api
+import com.wordnik.swagger.annotations.ApiOperation
+import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+import javax.validation.constraints.NotNull
+import java.util.concurrent.Callable
+
+import static org.springframework.web.bind.annotation.RequestMethod.PUT
+
+@Slf4j
+@RestController
+@RequestMapping('/api')
+@Api(value = "pairId", description = "Collects topics from blog data and propagates them to Collerators")
+class PairIdController {
+
+    @Autowired private PropagationWorker propagationWorker
+
+    
+    @RequestMapping(
+            value = '{pairId}',
+            method = PUT,
+            consumes = 'application/vnd.com.ofg.blog-topics-analyzer.v1+json',
+            produces = 'application/vnd.com.ofg.blog-topics-analyzer.v1+json')
+    @ApiOperation(value = "Async collecting and propagating of blog data for a given pairId",
+            notes = "This will handle topic extracting and pass results to Correlators")
+    Callable<Void> getTopicsFromBlogData(@PathVariable @NotNull long pairId, @RequestBody @NotNull String blogData) {
+        propagationWorker.collectAndPropagate(pairId, blogData)
+    }
+
+}
